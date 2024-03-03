9deef937e36a998aa7ea7817473a5e49f76e4a8c
Abooby1 <97899737+Abooby1@users.noreply.github.com>
2024-03-03 01:21:07 +0000

    commit

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 import JSONdb from 'simple-json-db'
 import Client from 'lyon'
+import AWS from "aws-sdk"
+setInterval(function() {
+	console.log('gi')
+}, 5000)
 
-var sendQuestion = true;
+const s3 = new AWS.S3()
+
+var sendQuestion = false;
 
 const questions = (new JSONdb('./questions.json')).storage
+let QuestionFile = await s3.getObject({
+		Bucket: "cyclic-tame-cyan-sockeye-toga-us-east-1",
+		Key: "questions.json",
+}).promise()
+//let questions = JSON.parse(QuestionFile)
 
 const client = await new Client({
 	userid: '63824e52d62f9d79c6459b40',
 	token: process.env['token']
 })
+
+await s3.putObject({
+		Body: JSON.stringify(questions),
+		Bucket: "cyclic-tame-cyan-sockeye-toga-us-east-1",
+		Key: "questions.json",
+}).promise()
 
 if(sendQuestion) {
 	let question = questions[Math.floor(Math.random() * questions.length)]
